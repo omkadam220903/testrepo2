c3ac8acabeb06358567c0c5bb8f5e41fcf23e2da
om220903 <omkadam87626@gmail.com>
2025-05-12 16:56:48 +0530

    My First Commit

---
+++ junit-example/src/main/java/com/capgemini/junit_example/App.java
@@ -0,0 +1,12 @@
+package com.capgemini.junit_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello om!");
+		System.out.println("saionara");
+	}
+}
